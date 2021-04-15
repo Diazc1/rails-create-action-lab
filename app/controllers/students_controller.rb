90e1f632a5d2091e36bc774aee2b235f7e8f6f69
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-15 14:21:19 -0700

    commit tree 8b52ec02dd636973a53ba941b3b88b0c816022fe

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,17 @@ class StudentsController < ApplicationController
   def new
   end
 
+
+  #creates a new student
+  #redirects to the new student
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
   end
 
 end
